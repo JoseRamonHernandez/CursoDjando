9c0c4d13da94b4b3978ac034d212db62aa294977
José Ramón Hernández Muñoz <joseramonhernandezmunoz20@gmail.com>
2022-06-03 18:22:36 -0600

    templates about portfolio contact

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,35 +1,13 @@
-from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+    def home(request):
+        return render(request, "core/home.html")
 
-html_base = """
-    <h1>Mi Web Personal</h1>
-    <ul>
-        <li><a href="/">Portada</a></li>
-        <li><a href="/about/">Acerca de</a></li>
-        <li><a href="/contact/">Contacto</a></li>
-    </ul>
-"""
+    def about(request):
+        return render(request, "core/about.html")
 
-def home(request):
-    return render(request, "core/home.html")
+    def portfolio(request):
+        return render(request, "core/portfolio.html")
 
-
-    
-def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Me llamo Ramón y me encanta Django!</p>
-    """)
-
-def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aquí dejo mi email y mis redes sociales:</p>
-        <ul>
-            <li><a href="joseramonhernandezmunoz20@gmail.com">Email</a></li>
-            <li><a href="https://github.com/JoseRamonHernandez">Github</a></li>
-            <li><a href="https://youtube.com">Youtube</a></li>
-        </ul>
-    """)
+    def contact(request):
+        return render(request, "core/contact.html")
