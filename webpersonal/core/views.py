88932b279294d616ce67f2a9aca2737db190d275
José Ramón Hernández Muñoz <joseramonhernandezmunoz20@gmail.com>
2022-06-03 18:27:24 -0600

    herencia plantillas templates

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,4 +1,16 @@
+from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
+    </ul>
+"""
 
     def home(request):
         return render(request, "core/home.html")
