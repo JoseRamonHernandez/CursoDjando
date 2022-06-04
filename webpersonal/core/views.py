c0ee9a69f5c163cae4c2bb9b456ec948e37dfc21
José Ramón Hernández Muñoz <joseramonhernandezmunoz20@gmail.com>
2022-06-03 18:06:45 -0600

    paginas portafolio contacto

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -8,6 +8,7 @@ html_base = """
     <ul>
         <li><a href="/">Portada</a></li>
         <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
     </ul>
 """
 
@@ -21,4 +22,15 @@ def about(request):
     return HttpResponse(html_base + """
         <h2>Acerca de</h2>
         <p>Me llamo Ramón y me encanta Django!</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí dejo mi email y mis redes sociales:</p>
+        <ul>
+            <li><a href="joseramonhernandezmunoz20@gmail.com">Email</a></li>
+            <li><a href="https://github.com/JoseRamonHernandez">Github</a></li>
+            <li><a href="https://youtube.com">Youtube</a></li>
+        </ul>
     """)
